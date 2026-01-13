f8869770b31052b1a0d8fa2571f74d4e5825345c
Satrap18 <kreza6517@gmail.com>
2026-01-13 20:49:05 +0330

    chore: update

--- Python/OOP/main.py
+++ Python/OOP/main.py
@@ -12,15 +12,23 @@
 # ice_cream.eat()
 
 
-class IceCream:
+# class IceCream:
 
-    def __init__(self):
-        self.scoops = 3
+#     def __init__(self):
+#         self.scoops = 3
 
-    def eat(self, scoops):
-        self.scoops -= scoops
+#     def eat(self, scoops):
+#         if self.scoops < scoops:
+#             print('Sorry this is amount is missing!')
+#         else:
+#             self.scoops -= scoops
 
-ice_cream = IceCream()
-print(ice_cream.scoops)
-ice_cream.eat(2)
-print(ice_cream.scoops)
+#     def add(self, scoops):
+#         self.scoops += scoops
+
+# ice_cream = IceCream()
+# print(ice_cream.scoops)
+# ice_cream.eat(4)
+# print(ice_cream.scoops)
+# ice_cream.add(3)
+# print(ice_cream.scoops)
