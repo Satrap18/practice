02c897f040672dc743f6c36d6e4cc769a113da1a
Satrap18 <kreza6517@gmail.com>
2026-01-15 14:24:47 +0330

    chore : the sixth stage of learning is over

--- Python/OOP/course-1/main.py
+++ Python/OOP/course-1/main.py
@@ -130,31 +130,86 @@
 
 # the fourth stage of learning is over #
 
-class Light:
+# class Light:
 
-    def __init__(self, sync=None):
-        self.on = False
-        self.sync = sync
+#     def __init__(self, sync=None):
+#         self.on = False
+#         self.sync = sync
     
-    def toggle(self):
-        self.on = not self.on
+#     def toggle(self):
+#         self.on = not self.on
         
-        if self.sync is not None:
-            self.sync.toggle()
+#         if self.sync is not None:
+#             self.sync.toggle()
 
-        print(self.on)
+#         print(self.on)
     
-    def is_on(self):
-        if self.on == False:
-            print('light is off!')
-        else:
-            print('light is on!')
+#     def is_on(self):
+#         if self.on == False:
+#             print('light is off!')
+#         else:
+#             print('light is on!')
 
-light1 = Light()
-light2 = Light(sync=light1)
-light2.toggle()
+# light1 = Light()
+# light2 = Light(sync=light1)
+# light2.toggle()
 
-light1.is_on()
-light2.is_on()
+# light1.is_on()
+# light2.is_on()
 
-# the fourth stage of learning is over #
+# the fiveth stage of learning is over #
+
+# class IceCream:
+
+#     max_scoops = 3
+
+#     def __init__(self):
+#         super().__init__()
+#         self.scoops = self.max_scoops
+
+#     def eat(self, scoops):
+#         if self.scoops < scoops:
+#             print('Sorry this is amount is missing!')
+#         else:
+#             self.scoops -= scoops
+
+#     def add(self, scoops):
+#         self.scoops += scoops
+
+#         if self.scoops > self.max_scoops:
+#             print('Too many scoops! dropped ice cream.')
+#             self.scoops = 0
+
+
+
+# class IceCreamTruck:
+
+#     def __init__(self):
+#         super().__init__()
+#         self.sold = 0
+ 
+#     def order(self, scoops):
+#         ice_cream = IceCream()
+#         self.add(ice_cream, scoops)
+#         return ice_cream
+
+
+#     def add(self, ice_cream, scoops):
+        
+#         ice_cream.add(scoops)
+#         self.sold += scoops 
+
+# class DeluxeIceCreamTruck(IceCreamTruck):
+
+#     def order(self, scoops):
+#         ice_cream = super().order(scoops)
+#         ice_cream.add(1)
+#         return ice_cream
+    
+# truck = DeluxeIceCreamTruck()
+# ice_cream  = truck.order(2)
+# print(ice_cream.scoops)
+# data = truck.sold
+# print(data)
+
+# the sixth stage of learning is over #
