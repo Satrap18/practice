65a9bf3f84af49f01d3b3b6d948dc762d844751a
Satrap18 <kreza6517@gmail.com>
2026-01-29 16:33:18 +0330

    feat: run flask on docker

--- Docker/app.py
+++ Docker/app.py
@@ -1,1 +1,11 @@
-print("Hello from Docker")
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return f"ENV: {os.getenv('APP_ENV', 'not-set')}"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
