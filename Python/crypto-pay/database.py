331a49d799cf749cf2b7519ffa5c1f8ec461142a
Satrap18 <kreza6517@gmail.com>
2026-02-13 12:29:26 +0330

    chore: fix bug

--- Python/crypto-pay/database.py
+++ Python/crypto-pay/database.py
@@ -1,4 +1,16 @@
 import sqlite3
+from tronpy import Tron
+from tronpy.providers import HTTPProvider
+from tronpy.keys import PrivateKey
+import os
+import hashlib
+
+
+def create_wallet():
+    client = Tron(HTTPProvider('https://api.nileex.io'))
+    private_key = PrivateKey(os.urandom(32))
+    wallet_address = private_key.public_key.to_base58check_address()
+    return private_key.hex(), wallet_address
 
 class DataBase:
 
@@ -12,21 +24,28 @@ class DataBase:
     def create_table(self):
 
         self.cur.execute('CREATE TABLE IF NOT EXISTS user (fullname TEXT, username TEXT, password TEXT, wallet TEXT)')
-        print('database create!')
 
-    def create_user(self, fullname, username, password, wallet):
+        # print('database create!')
 
-        data = (fullname, username, password, wallet)
+    def create_user(self, fullname, username, password):
+
+        private_key, wallet_address = create_wallet()
+
+        hashed_password = hashlib.sha256(password.encode()).hexdigest()
+
+        data = (fullname, username, hashed_password, wallet_address)
 
         self.cur.execute('INSERT INTO user (fullname, username, password, wallet) VALUES (?,?,?,?)', data)
         self.con.commit()
-        self.con.close()
 
-        print('User Create')
+        # print(f'User "{username}" created! Wallet: {wallet_address}')
+        # print(f'Private Key (hex): {private_key}')
 
     def login_user(self, username, password):
 
-        res = self.cur.execute(f"SELECT username FROM user WHERE username=? AND password=?", (username, password))
+        hashed_password = hashlib.sha256(password.encode()).hexdigest()
+
+        res = self.cur.execute(f"SELECT username FROM user WHERE username=? AND password=?", (username, hashed_password))
 
         row = res.fetchone()
 
@@ -46,6 +65,18 @@ class DataBase:
             return row[0]
         else:
             return None
+
+    def cheak_user(self, username):
+
+        data = self.cur.execute("SELECT username FROM user WHERE username=?", (username,))
+
+        cheak = data.fetchone()
+
+        if cheak:
+            return cheak[0]
+        else:
+            return None
+        
 
 if __name__ == "__main__":
     main = DataBase()
