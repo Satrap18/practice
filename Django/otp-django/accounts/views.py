9fd83f2223ed2aaa295ca19e2ac05a1b4f3c450b
Satrap18 <kreza6517@gmail.com>
2026-04-18 10:53:59 +0330

    chore:update

--- Django/otp-django/accounts/views.py
+++ Django/otp-django/accounts/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from django.views.generic import View
+from accounts.forms import LoginForms
+from django.contrib.auth import login, authenticate, logout
 # Create your views here.
 
 class Login(View):
 
     def get(self, request, *args, **kwargs):
-        return render(request, 'login.html')
+        form = LoginForms()
+        return render(request, 'login.html', {'form': form})
 
-
+    def post(self, request, *args, **kwargs):
+        form = LoginForms()
+        clean_data = form.cleaned_data
+        username = clean_data['username']
+        password = clean_data['password']
+        user = (username, password)
+        login(request, user)
 
 class Register(View):
 
