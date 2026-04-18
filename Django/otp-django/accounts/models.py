c0a81eda4bf94715d26196a3a4497302ebcbfe7e
Satrap18 <kreza6517@gmail.com>
2026-04-18 10:53:42 +0330

    feat: add models login

--- Django/otp-django/accounts/models.py
+++ Django/otp-django/accounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class LoginForms(models.Model):
+    
+    username = models.CharField(max_length=20)
+    email = models.EmailField()
+    phone_number = models.IntegerField()
+    passwords = models.CharField(max_length=15)
+    name = models.CharField(max_length=15)
+    lastname = models.CharField(max_length=15)
+
