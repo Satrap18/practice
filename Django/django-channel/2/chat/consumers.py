de9a15838f26bd94fc6af6fb8d3f3aa571055c68
Satrap18 <kreza6517@gmail.com>
2026-04-26 09:42:12 +0330

    feat: start new practice for channel django

---
+++ Django/django-channel/2/chat/consumers.py
@@ -0,0 +1,21 @@
+import json
+from channels.generic.websocket import WebsocketConsumer
+
+
+class ChatConsumer(WebsocketConsumer):
+
+    def connect(self):
+
+        self.accept()
+
+        self.send(text_data=json.dumps({
+            'type':'connect',
+            'message': 'You are connect'
+        }))
+        return super().connect()
+
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+
+        print('message:', message)
