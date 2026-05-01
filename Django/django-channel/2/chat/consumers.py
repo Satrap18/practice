e6dc175b9f4bc76aedd29144d73a21d53fb8a794
Satrap18 <kreza6517@gmail.com>
2026-05-02 00:48:37 +0330

    chore: fix bug for working

--- Django/django-channel/2/chat/consumers.py
+++ Django/django-channel/2/chat/consumers.py
@@ -5,17 +5,18 @@ from channels.generic.websocket import WebsocketConsumer
 class ChatConsumer(WebsocketConsumer):
 
     def connect(self):
-
         self.accept()
-
         self.send(text_data=json.dumps({
-            'type':'connect',
-            'message': 'You are connect'
+            'type': 'connect',
+            'message': 'You are connected'  
         }))
-        return super().connect()
 
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-
         print('message:', message)
+        
+        self.send(text_data=json.dumps({
+            'type': 'reply',
+            'message': f'You said: {message}'
+        }))
