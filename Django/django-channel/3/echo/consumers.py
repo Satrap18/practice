1fdf65439c968d702574c23dd42ebe9b841d93ad
Satrap18 <kreza6517@gmail.com>
2026-05-02 01:44:12 +0330

    feat: create first channel consumers file

--- Django/django-channel/3/echo/consumers.py
+++ Django/django-channel/3/echo/consumers.py
@@ -1,1 +1,23 @@
-ECHO is on.
+import json
+from channels.generic.websocket import WebsocketConsumer
+
+class EchoConsumers(WebsocketConsumer):
+
+    def connect(self):
+        print("=" * 50)
+        print("🟢 CONNECT METHOD CALLED! 🟢")
+        print("=" * 50)
+        self.accept()
+
+        self.send(text_data=json.dumps(
+            {
+                'type': 'connect',
+                'message': 'connect to echo server'
+            }
+        ))
+
+    def receive(self, text_data):
+        self.send(text_data=text_data)
+
+    def disconnect(self, code):
+        print(f'websocket connection closed by code:{code}')
