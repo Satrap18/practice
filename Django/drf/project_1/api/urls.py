968e0f26e72cf02cf503db5a2794894703e50d10
Satrap18 <kreza6517@gmail.com>
2026-07-24 16:54:36 +0330

    chore: added delete and update and complete CURD with functions

--- Django/drf/project_1/api/urls.py
+++ Django/drf/project_1/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from api.views import user_profile
+from api.views import user_profile, update_user_proflie
 
 urlpatterns = [
-    path("user", user_profile, name="user_profile")
+    path("user", user_profile, name="user_profile"),
+    path("user/<id>/", update_user_proflie, name="update_user_profile"),
 ]
--- Django/drf/project_1/api/views.py
+++ Django/drf/project_1/api/views.py
@@ -24,4 +24,31 @@ def user_profile(request):
         serializer_data.save()
         return Response(serializer_data.data, status=status.HTTP_201_CREATED)
  
- 
+
+@api_view(['put', 'delete'])
+def update_user_proflie(request, id):
+
+    if request.method == "PUT":    
+        user_profile_obj = UserProfile.objects.get(id=id)
+        user_data = request.data
+            
+        serilizer_data = UserProfileSerializers(
+            instance = user_profile_obj,
+            data = user_data
+        )
+            
+        serilizer_data.is_valid(raise_exception=True)
+            
+        serilizer_data.update(
+            instance=user_profile_obj,
+            validated_data=serilizer_data.validated_data
+        )
+            
+        return Response(serilizer_data.data, status=status.HTTP_200_OK)
+    
+    if request.method == "DELETE":
+        
+        user_profile_obj = UserProfile.objects.get(id=id)
+        user_profile_obj.delete()
+        
+        return Response(status=status.HTTP_204_NO_CONTENT)
