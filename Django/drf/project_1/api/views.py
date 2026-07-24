0e6134a21efec672d3f8243874f41eee07d5c235
Satrap18 <kreza6517@gmail.com>
2026-07-24 13:44:30 +0330

    chore: update code

--- Django/drf/project_1/api/views.py
+++ Django/drf/project_1/api/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from api.models import UserProfile
-from api.serializers import UserProfileSerializers
+from api.serializers import UserProfileSerializers, CreateUserProfileSerializers
 # Create your views here.
 
 @api_view(['get', 'post'])
 def user_profile(request):
     
-    return Response({"data": "ok"})
+    if request.method == "GET":
+        data_user = UserProfile.objects.all()
+        serializer_data = UserProfileSerializers(data_user, many=True)
+        
+        return Response(serializer_data.data)
+    
+    if request.method == "POST":
+        
+        data_user = request.data
+        serializer_data = CreateUserProfileSerializers(data=data_user)
+        
+        serializer_data.is_valid(raise_exception=True)
+        serializer_data.save()
+        return Response(serializer_data.data, status=status.HTTP_201_CREATED)
+ 
+ 
