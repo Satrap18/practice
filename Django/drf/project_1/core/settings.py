cf4db483f3f32f05522bda66c9ed144528848289
Satrap18 <kreza6517@gmail.com>
2026-07-24 11:11:07 +0330

    chore: added api app to install app django

--- Django/drf/project_1/core/settings.py
+++ Django/drf/project_1/core/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # Another App #
+    'rest_framework',
+    
+    # My App #
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
