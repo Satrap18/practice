4268653f0f71587499593d60802a2581f8c8af4a
Satrap18 <kreza6517@gmail.com>
2026-07-24 11:30:09 +0330

    chore: added simple views for user profile with drf

--- Django/drf/project_1/api/views.py
+++ Django/drf/project_1/api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from api.models import UserProfile
+from api.serializers import UserProfileSerializers
 # Create your views here.
+
+@api_view(['get', 'post'])
+def user_profile(request):
+    
+    return Response({"data": "ok"})
