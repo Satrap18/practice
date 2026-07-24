d604014a4b3ae0e3def436e8ea8d69d885ecf9b6
Satrap18 <kreza6517@gmail.com>
2026-07-24 11:14:28 +0330

    feat: added simple models user profile

--- Django/drf/project_1/api/models.py
+++ Django/drf/project_1/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class UserProfile(models.Model):
+    
+    name = models.CharField(max_length=50)
+    age = models.PositiveIntegerField()
+    bio = models.TextField()
+    email = models.EmailField(max_length=254)
+    
+    def __str__(self) -> str:
+        return self.name
